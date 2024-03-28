14feeb4ec74bf4367f9c9ddfdbc368a7cd084d05
Diego Pereiro Rodriguez <58935004+dpereiro91@users.noreply.github.com>
2024-03-28 14:30:31 +0000

    Switch from motuclient to copernicusmarine

--- sst/copernicus.py
+++ sst/copernicus.py
@@ -1,21 +1,48 @@
-from motu import motu
+import copernicusmarine as cm
 import os
 
-def Copernicus_Marine_Service(USER, PSWD, PRODUCT, SERVICE, 
-    localpath, filename, lonmin, lonmax, latmin, latmax, idate, edate, var, mode):
+from log import set_logger, now
+logger = set_logger()
+
+def Copernicus_Marine_Service_Download(user, pswd, dataset, 
+        localpath, filename, lonmin, lonmax, latmin, latmax, idate, edate, var):
     
     '''
             This function downloads, as a NetCDF file, the variable from the
-            specified product and service. It uses motu-client
+            specified dataset. 
     '''
 
+    f = localpath + filename;
+        
     for i in range(5): # Try up to 5 times to download from Copernicus Marine Service
     
-        # Submit request to the motu client
-        f = motu(USER, PSWD, SERVICE, PRODUCT, localpath, filename, 
-             lonmin, lonmax, latmin, latmax, idate, edate, var, mode)
-        
+        logger.info(f'{now()} Trial {i} to download SST data')
+
+        cm.subset(
+                username=user,
+                password=pswd,
+                dataset_id=dataset,
+                output_directory=localpath,
+                output_filename=filename,
+                variables=[var],
+                minimum_longitude=lonmin,
+                maximum_longitude=lonmax,
+                minimum_latitude=latmin,
+                maximum_latitude=latmax,
+                start_datetime=idate,
+                end_datetime=edate,
+                force_download=True
+                )
+
         if os.path.isfile(f): # File downloaded successfully. Leave loop...
-            break
+            logger.info(f'{now()}   Successfully downloaded file {f}'); break
         else: # Download failed. Retry...
-            continue
+            logger.info(f'{now()}   Download failed!'); continue
+        
+    if os.path.isfile(f):
+        
+        logger.info(f'{now()} Reading local NetCDF file...') 
+        
+    else: # If, after trying 5 times, file is still unavailable, return NaN
+        
+        logger.info(f'{now()} Unable to download Copernicus Marine Service file')
