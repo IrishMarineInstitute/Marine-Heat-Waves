0db5a4199af169a21d5d06f160b15f0f8f8aa350
Diego Pereiro Rodriguez <58935004+dpereiro91@users.noreply.github.com>
2024-03-28 14:21:11 +0000

    Switch from motuclient to copernicusmarine

--- ostia/ostia.py
+++ ostia/ostia.py
@@ -13,13 +13,14 @@ def OSTIA(config, idate):
     s, n = float(config['s']), float(config['n'])
         
     # OSTIA associated product and service ID's
-    PRODUCT, SERVICE = config['product-id'], config['service-id']
+    copernicus_dataset = config['dataset-id']
+
 
     # Set local directory to download NetCDF files to 
     path = '/tmp/'     
 
-    # Set variable to download and mode to Near-Real-Time
-    var, mode = 'analysed_sst', 'nrt'
+    # Set variable to download 
+    var = 'analysed_sst'
 
     # If possible, download until today
     edate = date.today()
@@ -27,8 +28,12 @@ def OSTIA(config, idate):
     # File name to download 
     file = 'OSTIA-TMP.nc'
 
+    # Convert dates to strings
+    idate = idate.strftime('%Y-%m-%dT%H:%M:%S')
+    edate = edate.strftime('%Y-%m-%dT%H:%M:%S')
+
     # Download
-    copernicus.Copernicus_Marine_Service(USER, PSWD, PRODUCT, SERVICE,
-        path, file, w, e, s, n, idate, edate, var, mode)
+    copernicus.Copernicus_Marine_Service_Download(USER, PSWD, copernicus_dataset, 
+        path, file, w, e, s, n, idate, edate, var)
 
     return path + file
